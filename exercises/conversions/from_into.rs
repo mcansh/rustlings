49d674bc72c1834355506abf0b078780129cf852
Logan McAnsh <logan@mcan.sh>
2024-03-19 18:14:31 -0400

    x
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-	fn from(s: &str) -> Person {}
+	fn from(s: &str) -> Person {
+		if s.len() == 0 {
+			return Person::default();
+		}
+		let parts: Vec<&str> = s.split(',').collect();
+		let name = parts[0].to_string();
+		if name.len() == 0 {
+			return Person::default();
+		}
+		let age = match parts.get(1) {
+			Some(age) =>
+				match age.parse::<usize>() {
+					Ok(age) => age,
+					Err(_) => {
+						return Person::default();
+					}
+				}
+			None => {
+				return Person::default();
+			}
+		};
+		Person { name, age }
+	}
 }
 
 fn main() {
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
 	ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -51,7 +49,32 @@ enum ParsePersonError {
 
 impl FromStr for Person {
 	type Err = ParsePersonError;
-	fn from_str(s: &str) -> Result<Person, Self::Err> {}
+	fn from_str(s: &str) -> Result<Person, Self::Err> {
+		if s.len() == 0 {
+			return Err(ParsePersonError::Empty);
+		}
+		let parts: Vec<&str> = s.split(',').collect();
+		if parts.len() != 2 {
+			return Err(ParsePersonError::BadLen);
+		}
+		let name = parts[0].to_string();
+		if name.len() == 0 {
+			return Err(ParsePersonError::NoName);
+		}
+		let age = match parts.get(1) {
+			Some(age) =>
+				match age.parse::<usize>() {
+					Ok(age) => age,
+					Err(e) => {
+						return Err(ParsePersonError::ParseInt(e));
+					}
+				}
+			None => {
+				return Err(ParsePersonError::BadLen);
+			}
+		};
+		Ok(Person { name, age })
+	}
 }
 
 fn main() {
